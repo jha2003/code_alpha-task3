3eeaed251dcef73f8585b8f2b7818b06088f3ed2
Soni Jha <168816563+jha2003@users.noreply.github.com>
2024-08-09 11:25:44 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+// Book data
+let books = [
+    { id: 1, title: "Book 1", author: "Author 1", category: "Category 1" },
+    { id: 2, title: "Book 2", author: "Author 2", category: "Category 2" },
+    { id: 3, title: "Book 3", author: "Author 3", category:"category 2"}
+];
